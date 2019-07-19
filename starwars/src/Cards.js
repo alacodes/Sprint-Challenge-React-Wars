a171c777da50d3f0cd8f8304723e2f81af9d04b4
Tracie T <traciecodes@gmail.com>
2019-07-19 11:10:58 -0500

    Second commit: Added .js files for the cards and semantic ui grid

---
+++ starwars/src/Cards.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import { Cards } from 'semantic-ui-react';
+
+
+
+const Cards() {
+    //Define variables with state
+    //Return
+
+}
+
+export default Cards;
